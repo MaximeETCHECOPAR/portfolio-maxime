757d7413a0e420eb7b5a2ea6e9ac26b6e3ce4ca3
Etchecopar <metchecopar0@gmail.com>
2026-02-20 12:10:51 +0100

    ajout de Shadnc

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-maxime/', 
+  base: '/portfolio-maxime/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
